160abd469fee597f8b9dba624c6214a6748bb6eb
BNHDeveloper <gussisabrin@gmail.com>
2026-01-10 17:53:47 +0100

    cours 6 : TextFormField

---
+++ cours_6/lib/main.dart
@@ -0,0 +1,83 @@
+import 'dart:developer';
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      home: HomeScreen(), // proper widget
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<HomeScreen> {
+  GlobalKey<FormState> keyform = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("_________", style: TextStyle(color: Colors.deepOrange)),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Center(
+              child: Text(
+                "Choose an Operation system",
+                style: TextStyle(fontSize: 26),
+              ),
+            ),
+            SizedBox(height: 50),
+            Form(
+              key: keyform,
+              child: Column(
+                children: [
+                  TextFormField(
+                    autocorrect: true,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "You should write something";
+                      }
+                    },
+                  ),
+                  MaterialButton(
+                    child: Text("Add"),
+                    textColor: Colors.white,
+                    color: Colors.deepOrange,
+                    onPressed: () {
+                      if (keyform.currentState!.validate()) {
+                        print("good");
+                      } else {
+                        print("not valid");
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
